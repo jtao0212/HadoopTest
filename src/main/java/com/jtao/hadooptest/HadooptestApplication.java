eb98effbc95689f416c9d8b95d7e409c2ef4df0c
jiantao <jiantao@jiantaodeMacBook-Pro.local>
2020-06-10 21:51:24 +0800

    init

---
+++ src/main/java/com/jtao/hadooptest/HadooptestApplication.java
@@ -0,0 +1,13 @@
+package com.jtao.hadooptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadooptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HadooptestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jtao/hadooptest/HadooptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtao.hadooptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadooptestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
